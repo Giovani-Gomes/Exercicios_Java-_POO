58e689aec1529cbfe130615bf2b69a06b2a37e24
Giovani Gomes <giovanigomes2002@gmail.com>
2022-07-02 06:19:55 -0300

    Exercício 02
    Resolução do Exercício 2 Java POO e  adicionada a sua descrição.

---
+++ Exercicio 02/Livro.java
@@ -0,0 +1,9 @@
+package Exercicios.java;
+
+public class Livro {
+    String nome;
+    String autor;
+    String editora;
+    int qtdPaginas;
+    String categoria;
+}
---
+++ Exercicio 02/exercicio2.java
@@ -0,0 +1,23 @@
+package Exercicios.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Livro livro1 = new Livro();
+        livro1.nome = "Preço da Desonta";
+        livro1.autor = "Hiroshi Hirata";
+        livro1.qtdPaginas = 385 ;
+        livro1.categoria = "História em Quadrinho/Mangá";
+        livro1.editora = "Pipoca e Nanquin";
+
+        System.out.println("NOME - " + livro1.nome);
+        System.out.println("AUTOR - " + livro1.autor);
+        System.out.println("QUANTIDADE DE PÁGINAS - " + livro1.qtdPaginas);
+        System.out.println("CATEGORIA - " + livro1.categoria);
+        System.out.println("EDITORA - " + livro1.editora);
+
+
+    }
+}
