a76b278c7f22865af6928b4c65dcc170288f6be5
Giovani Gomes <giovanigomes2002@gmail.com>
2022-07-10 00:34:34 -0300

    Exercicio
    Resolução do Exercício 03 Variáveis e métodos estáticos.

---
+++ Exercício 03 - Variáves e Métodos Estáticos(static) /Calculadora.java
@@ -0,0 +1,44 @@
+package Exercicios.java;
+
+public class Calculadora {
+
+    public static int somar (int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int subtrair (int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int multiplicar (int num1, int num2){
+        return num1 * num2;
+    }
+    public static int potenciacao (int num1, int num2){
+
+        int total = 1;
+        for (int i=1; i<=num2; i++){
+            total *= num1;
+        }
+
+        return total;
+    }
+
+
+    public static int fatorial(int num){
+
+        if (num == 0){
+            return 1;
+        }
+
+        int total = 1;
+        for (int i=num; i>1; i--){
+            total *= i;
+        }
+
+        return total;
+    }
+
+    public static int dividir (int num1, int num2){
+        return num1 / num2;
+    }
+}
---
+++ Exercício 03 - Variáves e Métodos Estáticos(static) /main.java
@@ -0,0 +1,18 @@
+    package Exercicios.java;
+
+    import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+            imprimirTela(Calculadora.somar(7,10));
+            imprimirTela(Calculadora.subtrair(7,9));
+            imprimirTela(Calculadora.multiplicar(10,10));
+            imprimirTela(Calculadora.dividir(10,5));
+            imprimirTela(Calculadora.fatorial(7));
+            imprimirTela(Calculadora.potenciacao(7,8));
+        }
+        static void imprimirTela(int num){
+            System.out.println(num);
+        }
+    }
